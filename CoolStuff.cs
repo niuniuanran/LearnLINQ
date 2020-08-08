6981ab30b0315c653334b763cd1faffa8322910b
niuniuanran <irene.anran.niu@gmail.com>
2020-08-08 22:37:33 +1200

    Get average age of people

--- CoolStuff.cs
+++ CoolStuff.cs
@@ -121,6 +121,26 @@ namespace LearnLinq
             int min = integers.Min();
             Console.WriteLine($"Min: {min}");
 
+            SeparatingLine("More things to do with Aggregation");
+            List<Person> people = new List<Person>()
+            {
+                new Person("Tod", "Vachev", 1, 180, 26, Gender.Male),
+                new Person("John", "Johnson", 2, 170, 21, Gender.Male),
+                new Person("Anna", "Maria", 3, 150, 22, Gender.Female),
+                new Person("Kyle", "Wilson", 4, 164, 29, Gender.Male),
+                new Person("Anna", "Williams", 5, 164, 28, Gender.Male),
+                new Person("Maria", "Ann", 6, 160, 43, Gender.Female),
+                new Person("John", "Jones", 7, 160, 37, Gender.Female),
+                new Person("Samba", "TheLion", 8, 175, 33, Gender.Male),
+                new Person("Aaron", "Myers", 9, 182, 21, Gender.Male),
+                new Person("Aby", "Wood", 10, 165, 20, Gender.Female),
+                new Person("Maddie","Lewis",  11, 160, 19, Gender.Female),
+                new Person("Lara", "Croft", 12, 162, 18, Gender.Female)
+            };
+            double averageAge = people.Average(p => p.Age);
+            Console.WriteLine($"Average age: {averageAge}");
+
+
 
         }
 
