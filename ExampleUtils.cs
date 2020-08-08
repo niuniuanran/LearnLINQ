f4fcdc37e751fc08a96adf2bb376f49851712e7b
niuniuanran <irene.anran.niu@gmail.com>
2020-08-08 13:04:33 +1200

    Extract People, YoungPeople classes into separate file

---
+++ ExampleUtils.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnLinq
+{
+
+
+    public class YoungPerson
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int id;
+        private int height;
+        private int age;
+
+        private Gender gender;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                this.gender = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int id, int height, int age, Gender gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.Height = height;
+            this.Age = age;
+            this.Gender = gender;
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
